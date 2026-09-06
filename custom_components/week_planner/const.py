9d316c74cff3798578ba473160d63232cf3dcab0
MEbsen <35669569+MEbsen@users.noreply.github.com>
2026-09-06 19:11:22 +0200

    Set dev build version to 0.5.3-dev.2

--- custom_components/week_planner/const.py
+++ custom_components/week_planner/const.py
@@ -1,7 +1,7 @@
 """Constants for Week Planner."""
 DOMAIN = "week_planner"
 NAME = "Week Planner"
-VERSION = "0.5.3-dev"
+VERSION = "0.5.3-dev.2"
 
 CONF_WEATHER_ENTITY = "weather_entity"
 CONF_CALENDAR_ENTITIES = "calendar_entities"
